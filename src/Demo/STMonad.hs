63f91730a7cde22b59ea2c13aa5ff22a54e66b72
John Ky <newhoggy@gmail.com>
2014-09-30 17:12:48 +1000

    Ensure that cabal project builds the Demo.STMonad module. Some code demonstrating rank 2 polymorphism.

--- src/Demo/STMonad.hs
+++ src/Demo/STMonad.hs
@@ -1,7 +1,40 @@
+{-# LANGUAGE Rank2Types #-}
+
 module Demo.STMonad where
 
-encode :: [Int] -> String
-encode nums = show nums
+import Control.Monad
 
-decode :: String -> [Int]
-decode hashid = read hashid
+-- Type trickery to keep the cat in the bag
+data Bag s a = Bag a -- like ST 
+data Cat s a = Cat a -- like STRef 
+
+instance Monad (Bag s) where
+    (>>=) = bindBag
+    return = returnBag
+
+runBag :: forall a. (forall s. Bag s a) -> a
+runBag k = case k of Bag a -> a
+
+newCat :: a -> (forall s. Bag s (Cat s a))
+newCat x = Bag (Cat x)
+
+readCat :: forall s. forall a. (Cat s a -> Bag s a)
+readCat x = case x of (Cat v) -> Bag v
+
+bindBag :: Bag s a -> (a -> Bag s b) -> Bag s b
+bindBag s f = case s of Bag x -> f x
+
+returnBag :: a -> forall s. Bag s a 
+returnBag x = Bag x
+
+
+-- Works, cat stays in the bag
+test = runBag $ do
+    cat <- newCat "meow"
+    value <- readCat cat
+    return value
+
+-- --Doesn't compile - attempts to let the cat out
+fail = runBag $ do
+    cat <- newCat "meow"
+    return cat
